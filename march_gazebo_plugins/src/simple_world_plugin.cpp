d10759ce968c3fba64fcf28644ee0cd34c9ce19a
roel <roel@vossenopreis.nl>
2019-11-29 12:17:31 +0100

    Make test model plugin

--- march_gazebo_plugins/src/simple_world_plugin.cpp
+++ march_gazebo_plugins/src/simple_world_plugin.cpp
@@ -1,28 +1,17 @@
-#include <gazebo/common/Plugin.hh>
-#include <ros/ros.h>
+#include <gazebo/gazebo.hh>
 
 namespace gazebo
 {
     class WorldPluginTutorial : public WorldPlugin
     {
-    public:
-        WorldPluginTutorial() : WorldPlugin()
+    public: WorldPluginTutorial() : WorldPlugin()
         {
+            printf("Hello World #2!\n");
         }
 
-        void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
         {
-            // Make sure the ROS node for Gazebo has already been initialized
-            if (!ros::isInitialized())
-            {
-                ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-                                         << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
-                return;
-            }
-
-            ROS_INFO("Hello World!");
         }
-
     };
     GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
 }

fec8f3ccd50c43f8e43730496f1cf7f58d199e83
roel <roel@vossenopreis.nl>
2020-01-13 15:25:34 +0100

    Clean up controller code and change subgait to message

--- march_fake_sensor_data/src/FakeTemperatureData.cpp
+++ march_fake_sensor_data/src/FakeTemperatureData.cpp
@@ -31,10 +31,11 @@ std::vector<int> latest_temperatures;
 std::vector<float> ar_values;
 
 // Calculate the weighted average of the latest_temperatures
-double calculateArTemperature(std::vector<int> temperatures,
-                              std::vector<float> ar_values) {
+double calculateArTemperature(std::vector<int> temperatures, std::vector<float> ar_values)
+{
   double res = 0;
-  for (unsigned int i = 0; i < temperatures.size(); i++) {
+  for (unsigned int i = 0; i < temperatures.size(); i++)
+  {
     res += temperatures.at(i) * ar_values.at(i);
   }
   return res;
@@ -47,11 +48,12 @@ double calculateArTemperature(std::vector<int> temperatures,
  * @param config the config file with all the parameters
  * @param level A bitmask
  */
-void temperatureConfigCallback(
-    march_fake_sensor_data::TemperaturesConfig &config, uint32_t level) {
+void temperatureConfigCallback(march_fake_sensor_data::TemperaturesConfig& config, uint32_t level)
+{
   // Make sure there is always a possible interval between min and max
   // temperature.
-  if (config.min_temperature >= config.max_temperature) {
+  if (config.min_temperature >= config.max_temperature)
+  {
     config.max_temperature = config.min_temperature + 1;
   }
   min_temperature = config.min_temperature;
@@ -64,7 +66,8 @@ void temperatureConfigCallback(
  * @param end Highest number
  * @return The Random number
  */
-int randBetween(int start, int end) {
+int randBetween(int start, int end)
+{
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dis(start, end);
@@ -76,7 +79,8 @@ int randBetween(int start, int end) {
  * parameters
  * @param temperature_pub publish the temperature message with this publisher
  */
-void publishTemperature(const ros::Publisher &temperature_pub) {
+void publishTemperature(const ros::Publisher& temperature_pub)
+{
   int random_temperature = randBetween(min_temperature, max_temperature);
 
   // Update the vector with the latest temperatures by removing the first entry
@@ -84,8 +88,7 @@ void publishTemperature(const ros::Publisher &temperature_pub) {
   latest_temperatures.erase(latest_temperatures.begin());
   latest_temperatures.push_back(random_temperature);
 
-  double current_temperature =
-      calculateArTemperature(latest_temperatures, ar_values);
+  double current_temperature = calculateArTemperature(latest_temperatures, ar_values);
   sensor_msgs::Temperature msg;
   msg.temperature = current_temperature;
   msg.header.stamp = ros::Time::now();
@@ -98,24 +101,24 @@ void publishTemperature(const ros::Publisher &temperature_pub) {
  * @param name Name of the sensor
  * @return Full topic name
  */
-std::string createTopicName(const char *base, const char *name) {
+std::string createTopicName(const char* base, const char* name)
+{
   char slash[] = "/";
   // The buffer needs more space then the final string length.
   int extra_buffer_size = 100;
-  const int kArraySize = static_cast<const int>(
-      strlen(base) + strlen(slash) + strlen(name) + extra_buffer_size);
+  const int kArraySize = static_cast<const int>(strlen(base) + strlen(slash) + strlen(name) + extra_buffer_size);
   // Create a char array of the combined size of all three parts
   char full_topic[kArraySize];
-  int error_code =
-      snprintf(full_topic, kArraySize, "%s%s%s", base, slash, name);
-  if (error_code < 0 || error_code > kArraySize) {
-    ROS_ERROR("Error creating topic %s (error code %d)", full_topic,
-              error_code);
+  int error_code = snprintf(full_topic, kArraySize, "%s%s%s", base, slash, name);
+  if (error_code < 0 || error_code > kArraySize)
+  {
+    ROS_ERROR("Error creating topic %s (error code %d)", full_topic, error_code);
   }
   return full_topic;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "march_fake_sensor_data");
   ros::NodeHandle n;
   ros::Rate rate(10);
@@ -124,39 +127,41 @@ int main(int argc, char **argv) {
   ros::param::get("~max_temperature", max_temperature);
 
   int count = 0;
-  while (!n.hasParam("/march/joint_names")) {
+  while (!n.hasParam("/march/joint_names"))
+  {
     ros::Duration(0.5).sleep();
     count++;
-    if (count > 10) {
+    if (count > 10)
+    {
       ROS_ERROR("Failed to read the joint_names from the parameter server.");
-      throw std::runtime_error(
-          "Failed to read the joint_names from the parameter server.");
+      throw std::runtime_error("Failed to read the joint_names from the parameter server.");
     }
   }
 
   n.getParam("/march/joint_names", sensor_names);
 
   // Initialise autoregression variables.
-  latest_temperatures = {0, 0, 0, 0, 0, 0, 0};
-  ar_values = {0.1, 0.1, 0.1, 0.15, 0.15, 0.2, 0.2};
+  latest_temperatures = { 0, 0, 0, 0, 0, 0, 0 };
+  ar_values = { 0.1, 0.1, 0.1, 0.15, 0.15, 0.2, 0.2 };
 
   // Create a publisher for each sensor
-  for (std::string sensor_name : sensor_names) {
-    ros::Publisher temperature_pub = n.advertise<sensor_msgs::Temperature>(
-        createTopicName("/march/temperature", sensor_name.c_str()), 1000);
+  for (std::string sensor_name : sensor_names)
+  {
+    ros::Publisher temperature_pub =
+        n.advertise<sensor_msgs::Temperature>(createTopicName("/march/temperature", sensor_name.c_str()), 1000);
     temperature_publishers.push_back(temperature_pub);
   }
 
   // Make the temperature values dynamic reconfigurable
-  dynamic_reconfigure::Server<march_fake_sensor_data::TemperaturesConfig>
-      server;
-  dynamic_reconfigure::Server<
-      march_fake_sensor_data::TemperaturesConfig>::CallbackType f;
+  dynamic_reconfigure::Server<march_fake_sensor_data::TemperaturesConfig> server;
+  dynamic_reconfigure::Server<march_fake_sensor_data::TemperaturesConfig>::CallbackType f;
   server.setCallback(boost::bind(&temperatureConfigCallback, _1, _2));
 
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     // Loop through all publishers
-    for (ros::Publisher temperature_pub : temperature_publishers) {
+    for (ros::Publisher temperature_pub : temperature_publishers)
+    {
       publishTemperature(temperature_pub);
     }
 
--- march_gazebo_plugins/src/com_controller.cpp
+++ march_gazebo_plugins/src/com_controller.cpp
@@ -4,97 +4,92 @@
 #include <ros/callback_queue.h>
 #include <ros/ros.h>
 
-namespace gazebo {
-
-class ComController : public ModelPlugin {
-
+namespace gazebo
+{
+class ComController : public ModelPlugin
+{
 public:
-  void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+  {
     // Make sure the ROS node for Gazebo has already been initialized
-    if (!ros::isInitialized()) {
-      ROS_FATAL_STREAM(
-          "A ROS node for Gazebo has not been initialized, unable to load "
-          "plugin. "
-          << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in "
-             "the gazebo_ros package)");
+    if (!ros::isInitialized())
+    {
+      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load "
+                       "plugin. "
+                       << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in "
+                          "the gazebo_ros package)");
       return;
     }
 
-    // Store the pointer to the model
+    // Initialise variables
     this->model = _parent;
-    this->leg_leg_distance = 0.5;
-    this->swing_step_size = 0.7;
-    this->subgait_duration = 1.5;
-    this->open_step_size = 0.4;
+    this->foot_left = this->model->GetLink("ankle_plate_left");
+    this->foot_right = this->model->GetLink("ankle_plate_right");
 
+    this->swing_step_size = 0.7;  // This estimate will be adjusted every step
     this->subgait_name = "home_stand";
+
     this->p_pitch = 60;
     this->d_pitch = 10;
     this->p_roll = 150;
     this->d_roll = 7.5;
     this->p_yaw = 500;
     this->d_yaw = 25;
-    this->subgait_start_time = 0;
+
     this->error_x_last_timestep = 0;
     this->error_y_last_timestep = 0;
     this->error_yaw_last_timestep = 0;
 
-    // Apply a small linear velocity to the model.
-    for (auto const &i : this->model->GetLinks()) {
-      std::cout << i->GetName() << "\n";
-    }
-
-    this->foot_left = this->model->GetLink("ankle_plate_left");
-    this->foot_right = this->model->GetLink("ankle_plate_right");
-    this->base_link = this->model->GetLink("base_link");
-
     // Listen to the update event. This event is broadcast every
     // simulation iteration.
-    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-        std::bind(&ComController::OnUpdate, this));
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&ComController::OnUpdate, this));
 
     // Create our ROS node.
     this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
 
     // Create a named topic, and subscribe to it.
-    ros::SubscribeOptions so =
-        ros::SubscribeOptions::create<march_shared_resources::GaitActionGoal>(
-            "/march/gait/schedule/goal", 1,
-            boost::bind(&ComController::OnRosMsg, this, _1), ros::VoidPtr(),
-            &this->rosQueue);
+    ros::SubscribeOptions so = ros::SubscribeOptions::create<march_shared_resources::GaitActionGoal>(
+        "/march/gait/schedule/goal", 1, boost::bind(&ComController::OnRosMsg, this, _1), ros::VoidPtr(),
+        &this->rosQueue);
     this->rosSub = this->rosNode->subscribe(so);
 
     // Spin up the queue helper thread.
-    this->rosQueueThread =
-        std::thread(std::bind(&ComController::QueueThread, this));
+    this->rosQueueThread = std::thread(std::bind(&ComController::QueueThread, this));
   }
 
 public:
-  void OnRosMsg(const march_shared_resources::GaitActionGoalConstPtr &_msg) {
-    std::cout << "Its a message!" << '\n';
-    std::cout << _msg->goal.current_subgait.name << '\n';
+  void OnRosMsg(const march_shared_resources::GaitActionGoalConstPtr& _msg)
+  {
+    if (this->subgait_name == "right_open" or this->subgait_name == "right_swing" or this->subgait_name == "left_swing")
+    {
+      this->swing_step_size = 0.8 * this->swing_step_size + 0.4 * std::abs(this->foot_right->WorldPose().Pos().X() -
+                                                                           this->foot_left->WorldPose().Pos().X());
+    }
+
     this->subgait_name = _msg->goal.current_subgait.name;
     this->subgait_duration =
-        _msg->goal.current_subgait.duration.sec +
-        0.000000001 * _msg->goal.current_subgait.duration.nsec;
+        _msg->goal.current_subgait.duration.sec + 0.000000001 * _msg->goal.current_subgait.duration.nsec;
     this->subgait_start_time = this->model->GetWorld()->SimTime().Double();
-    std::cout << this->subgait_duration << '\n';
   }
 
   /// \brief ROS helper function that processes messages
 private:
-  void QueueThread() {
+  void QueueThread()
+  {
     static const double timeout = 0.01;
-    while (this->rosNode->ok()) {
+    while (this->rosNode->ok())
+    {
       this->rosQueue.callAvailable(ros::WallDuration(timeout));
     }
   }
 
   // Called by the world update start event
 private:
-  double GetMass() {
+  double GetMass()
+  {
     double mass;
-    for (auto const &link : this->model->GetLinks()) {
+    for (auto const& link : this->model->GetLinks())
+    {
       mass += link->GetInertial()->Mass();
     }
     return mass;
@@ -102,9 +97,11 @@ private:
 
   // Called by the world update start event
 private:
-  ignition::math::v4::Vector3<double> GetCom() {
+  ignition::math::v4::Vector3<double> GetCom()
+  {
     ignition::math::v4::Vector3<double> com(0.0, 0.0, 0.0);
-    for (auto const &link : this->model->GetLinks()) {
+    for (auto const& link : this->model->GetLinks())
+    {
       com += link->WorldCoGPose().Pos() * link->GetInertial()->Mass();
     }
     return com / this->GetMass();
@@ -112,106 +109,75 @@ private:
 
   // Called by the world update start event
 public:
-  void OnUpdate() {
+  void OnUpdate()
+  {
     // Note: the exo moves in the negative x direction, and the right leg is in
     // the positive y direction
-    double time = this->model->GetWorld()->SimTime().Double();
-
+    double time_since_start = this->model->GetWorld()->SimTime().Double() - subgait_start_time;
     auto model_com = this->GetCom();
-    auto foot_left_pose = this->foot_left->WorldCoGPose().Pos();
-    auto foot_right_pose = this->foot_right->WorldCoGPose().Pos();
 
-    double goal_position_x = 0.5 * (foot_left_pose.X() + foot_right_pose.X()) - 0.05;
-    double goal_position_y;
+    // Left foot is stable unless subgait name starts with left
+    auto stable_foot_pose = this->foot_left->WorldCoGPose().Pos();
+    std::string stable_side = "left";
+    if (this->subgait_name.substr(0, 4) == "left")
+    {
+      stable_foot_pose = this->foot_right->WorldCoGPose().Pos();
+      stable_side = "right";
+    }
 
-      if (subgait_name == "home_stand"){
-                goal_position_x = foot_left_pose.X();
-                goal_position_y = foot_left_pose.Y();
-      }
-      else if (subgait_name == "right_open") {
-                goal_position_x = foot_left_pose.X() - 0.5 * (time - subgait_start_time) *
-                                                       open_step_size / subgait_duration;
-                goal_position_y = foot_left_pose.Y();
-      }
-      else if (subgait_name == "right_swing") {
-                goal_position_x = foot_left_pose.X() + 0.25 * swing_step_size -
-                                                       0.5 * (time - subgait_start_time) * swing_step_size / subgait_duration;
-                goal_position_y = foot_left_pose.Y();
-      }
-      else if (subgait_name == "left_swing") {
-                goal_position_x = foot_right_pose.X() + 0.25 * swing_step_size -
-                                                        0.5 * (time - subgait_start_time) * swing_step_size / subgait_duration;
-                goal_position_y = foot_right_pose.Y();
-      }
-      else {
-          std::cout << subgait_name << '\n';
-      }
+    // Goal position is determined from the location of the stable foot
+    double goal_position_x = stable_foot_pose.X();
+    double goal_position_y = stable_foot_pose.Y();
 
+    // Start goal position a quarter step size behind the stable foot
+    // Move the goal position forward with v = 0.5 * swing_step_size/subgait_duration
+    if (this->subgait_name.substr(this->subgait_name.size() - 4) == "open")
+    {
+      goal_position_x += -0.25 * time_since_start * swing_step_size / subgait_duration;
+    }
+    else if (this->subgait_name.substr(this->subgait_name.size() - 5) == "swing")
+    {
+      goal_position_x += 0.25 * swing_step_size - 0.5 * time_since_start * swing_step_size / subgait_duration;
+    }
+    else if (this->subgait_name.substr(this->subgait_name.size() - 5) == "close")
+    {
+      goal_position_x += 0.25 * swing_step_size - 0.25 * time_since_start * swing_step_size / subgait_duration;
+    }
 
-
-
-
-//    if (subgait_name == "home_stand") {
-//      goal_position_y =
-//          foot_left_pose.Y();
-//    } else if (subgait_name == "right_open") {
-//        goal_position_y =
-//                foot_left_pose.Y();
-//    } else if (subgait_name == "right_swing") {
-//        goal_position_y =
-//                foot_left_pose.Y();
-//    } else if (subgait_name == "left_swing") {
-//      goal_position_y =
-//                foot_right_pose.Y();
-//    } else {
-//      std::cout << subgait_name << '\n';
-//    }
-
-    double goal_roll = 0.5 * (foot_left_pose.Y() + foot_right_pose.Y());
     double error_x = model_com.X() - goal_position_x;
     double error_y = model_com.Y() - goal_position_y;
-    double error_yaw = foot_left->WorldPose().Rot().Z();
+    double error_yaw = this->foot_left->WorldPose().Rot().Z();
 
-    double T_pitch = -this->p_pitch * error_x -
-                     this->d_pitch * (error_x - this->error_x_last_timestep);
-    double T_roll = this->p_roll * error_y +
-                    this->d_roll * (error_y - this->error_y_last_timestep);
-    double T_yaw = -this->p_yaw * error_yaw -
-                   this->d_yaw * (error_yaw - this->error_yaw_last_timestep);
+    double T_pitch = -this->p_pitch * error_x - this->d_pitch * (error_x - this->error_x_last_timestep);
+    double T_roll = this->p_roll * error_y + this->d_roll * (error_y - this->error_y_last_timestep);
+    double T_yaw = -this->p_yaw * error_yaw - this->d_yaw * (error_yaw - this->error_yaw_last_timestep);
+
+    const ignition::math::v4::Vector3<double> torque_all(0, T_pitch, T_yaw);  // -roll, pitch, -yaw
+    const ignition::math::v4::Vector3<double> torque_stable(T_roll, 0, 0);    // -roll, pitch, -yaw
+
+    for (auto const& link : this->model->GetLinks())
+    {
+      link->AddTorque(torque_all);
+
+      // Apply rolling torque to all links in the stable leg
+      if (link->GetName().find(stable_side) != std::string::npos)
+      {
+        link->AddTorque(torque_stable);
+      }
+    }
 
     this->error_x_last_timestep = error_x;
     this->error_y_last_timestep = error_y;
     this->error_yaw_last_timestep = error_yaw;
-
-    const ignition::math::v4::Vector3<double> torque_all(
-        0, T_pitch, T_yaw); // -roll, pitch, -yaw
-    const ignition::math::v4::Vector3<double> torque_stable(
-        T_roll, 0, 0); // -roll, pitch, -yaw
-
-    for (auto const &link : this->model->GetLinks()) {
-      link->AddTorque(torque_all);
-      if (subgait_name == "left_swing") {
-        if (link->GetName().find("right") != std::string::npos) {
-          link->AddTorque(torque_stable);
-        }
-      } else {
-        if (link->GetName().find("left") != std::string::npos) {
-          link->AddTorque(torque_stable);
-        }
-      }
-    }
   }
 
 private:
   physics::ModelPtr model;
   physics::LinkPtr foot_left;
   physics::LinkPtr foot_right;
-  physics::LinkPtr base_link;
   double subgait_start_time;
   double swing_step_size;
   double subgait_duration;
-  double open_step_size;
-  double leg_leg_distance;
   double p_yaw;
   double d_yaw;
   double p_pitch;
@@ -241,4 +207,4 @@ private:
 
 // Register this plugin with the simulator
 GZ_REGISTER_MODEL_PLUGIN(ComController)
-} // namespace gazebo
+}  // namespace gazebo

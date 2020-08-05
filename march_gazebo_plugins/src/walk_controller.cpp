907030c3b746c074c326c501a3374b0865c5c706
roel <roel@vossenopreis.nl>
2020-08-05 14:18:06 +0200

    Fix com controller

--- march_gazebo_plugins/src/walk_controller.cpp
+++ march_gazebo_plugins/src/walk_controller.cpp
@@ -9,7 +9,7 @@ WalkController::WalkController(physics::ModelPtr model) : ObstacleController(mod
 {
   this->swing_step_size_ = 0.7;  // This estimate will be adjusted every step
 
-  this->p_pitch_ = 120;
+  this->p_pitch_ = 300;
   this->d_pitch_ = 40000;
   this->p_roll_ = 150;
   this->d_roll_ = 40000;
